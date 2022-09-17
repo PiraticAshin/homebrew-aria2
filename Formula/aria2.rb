7ab2bcc9893cc2396121926eeb05aba07725cdc8
piercechan <pirashin@outlook.com>
2022-09-17 17:50:16 +0800

    修改依赖项顺序。

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -7,8 +7,8 @@ class Aria2 < Formula
   license "GPL-2.0-or-later"
 
   depends_on "pkg-config" => :build
-  depends_on "gettext"
   depends_on "c-ares"  # Homebrew提供的aria2不提供c-ares依赖。
+  depends_on "gettext"
   depends_on "libssh2"
   depends_on "sqlite"
 
